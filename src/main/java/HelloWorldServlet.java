9c3dd51fa65d5e68f05aaef444ee429313587b79
Jacob Barrow <jacob.barrow@gmail.com>
2020-01-07 11:31:17 -0600

    completed servlet exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,34 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    int hitCount = 0;
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = req.getParameter("name");
+        if (name == null || name.isEmpty()) {
+            res.setContentType("text/html");
+            PrintWriter out = res.getWriter();
+            out.println("<h1>Hey, world!</h1>");
+            hitCount++;
+            out.println("<h4>Page Views: " + hitCount);
+
+        } else {
+            res.setContentType("text/html");
+            PrintWriter out = res.getWriter();
+            out.println("<h1>Hey, " + name + " </h1>");
+            hitCount++;
+            out.println("<h4>Page Views: " + hitCount);
+        }
     }
 }
+
