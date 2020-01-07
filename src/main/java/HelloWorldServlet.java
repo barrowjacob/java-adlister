ed8169aa32ce53bfbad41f1d3521211dfbc4dc2a
Jacob Barrow <jacob.barrow@gmail.com>
2020-01-07 14:00:40 -0600

    completed servlet exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,27 +8,33 @@ import java.io.PrintWriter;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
 
-    int hitCount = 0;
 
 
+        int hitCount = 0;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String reset = req.getParameter("reset");
         String name = req.getParameter("name");
-        if (name == null || name.isEmpty()) {
-            res.setContentType("text/html");
-            PrintWriter out = res.getWriter();
-            out.println("<h1>Hey, world!</h1>");
-            hitCount++;
-            out.println("<h4>Page Views: " + hitCount);
+                res.setContentType("text/html");
+                PrintWriter out = res.getWriter();
+            if (name == null || name.isEmpty()) {
+                out.println("<h1>Hey, world!</h1>");
+                hitCount++;
+                out.println("<h4>Page Views: " + hitCount);
+            }else {
+                out.println("<h1>Hey, " + name + " </h1>");
+                hitCount++;
+                out.println("<h4>Page Views: " + hitCount);
+            }
+            if (reset != null && !reset.isEmpty()) {
+                hitCount = 0;
+                out.println("<h1>Reset Page.");
+                out.println("<h4>Page Views: " + hitCount);
+            } else {
 
-        } else {
-            res.setContentType("text/html");
-            PrintWriter out = res.getWriter();
-            out.println("<h1>Hey, " + name + " </h1>");
-            hitCount++;
-            out.println("<h4>Page Views: " + hitCount);
+            }
         }
     }
-}
+
 
